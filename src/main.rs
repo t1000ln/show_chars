1a72a1615d664fdbae1145c2a8ce9d7da657fbfd
t1000ln <t1000_ln@aliyun.com>
2024-03-30 00:31:19 +0800

    增加平台字体适配。

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,9 @@ mod fltk_color;
 mod ansi_8_color;
 mod emoji;
 
+#[cfg(target_os = "linux")]
 use std::path::PathBuf;
+
 use fltk::app::App;
 use fltk::enums::Font;
 use crate::ansi_8_color::init_ansi_8_color;
@@ -15,13 +17,27 @@ use crate::ui_loader::UserInterface;
 
 
 fn main() {
-    let app_ins = App::default();
-    let mut font_path = PathBuf::from("resource");
-    font_path.push("NotoColorEmoji.ttf");
-    if Font::load_font(font_path).is_ok() {
-        Font::set_font(Font::Symbol, "Noto Color Emoji");
-        // println!("{:#?}", app::get_font_names());
+    let app_ins = App::default().load_system_fonts();
+
+
+    #[cfg(target_os = "windows")]
+    {
+        Font::set_font(Font::Symbol, " Segoe UI Emoji");
+        Font::set_font(Font::Screen, " Consolas");
+        Font::set_font(Font::ScreenBold, "BConsolas");
+
     }
+
+    #[cfg(target_os = "linux")]
+    {
+        let mut font_path = PathBuf::from("resource");
+        font_path.push("NotoColorEmoji.ttf");
+        if Font::load_font(font_path).is_ok() {
+            Font::set_font(Font::Symbol, "Noto Color Emoji");
+            println!("{:#?}", app::get_font_names());
+        }
+    }
+
 
     let mut ui: UserInterface = UserInterface::make_window();
 
